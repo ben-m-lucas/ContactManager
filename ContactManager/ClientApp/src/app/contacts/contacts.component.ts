671cdb13fcfdc326b8888e636b2d81ed4557c295
Ben Lucas <ben@lucasfamily-online.com>
2018-12-08 16:25:59 -0800

    implement adding name

--- ContactManager/ClientApp/src/app/contacts/contacts.component.ts
+++ ContactManager/ClientApp/src/app/contacts/contacts.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-contacts',
@@ -7,11 +8,37 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ContactsComponent {
   public contacts: Contact[];
+  public form: FormGroup = new FormGroup({
+    surname: new FormControl(),
+    givenName: new FormControl()
+  });
+
+  private http: HttpClient;
+  private baseUrl: string;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<Contact[]>(baseUrl + 'api/Contacts').subscribe(result => {
+    this.http = http;
+    this.baseUrl = baseUrl;
+    this.loadContacts();
+  }
+
+  loadContacts() {
+    this.http.get<Contact[]>(this.baseUrl + 'api/Contacts').subscribe(result => {
       this.contacts = result;
     }, error => console.error(error));
+  }
+
+  addContact() {
+    const contactToAdd: Contact = {
+      surname: this.form.get('surname').value,
+      givenName: this.form.get('givenName').value
+    };
+
+    this.http.post<Contact>(this.baseUrl + 'api/Contacts', contactToAdd).subscribe(result => {
+
+      this.form.setValue({ surname: '', givenName: '' });
+      this.loadContacts();
+    });
   }
 }
 
